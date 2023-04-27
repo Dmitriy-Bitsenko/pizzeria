479a3da58f6811ca80963f5b4ce691d8eb69382d
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2023-04-27 18:52:20 +0500

    added css and bootstrap

--- pizza/models.py
+++ pizza/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Pizza(models.Model):
-    name = models.CharField(max_length=100, null=True)
-    description = models.TextField(blank=True, null=False)
-    price = models.FloatField()
-    date_create = models.DateField(auto_now_add=True)
-    date_update = models.DateField(auto_now=True)
-    photo = models.ImageField(upload_to='image%Y/%m/%d', null=False)
-    exist = models.BooleanField(default=True, null=True)
+    name = models.CharField(default='pizza', max_length=100, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    price = models.FloatField(default=10, verbose_name='Цена')
+    date_create = models.DateField(auto_now_add=True, verbose_name='Дата добавления записи')
+    date_update = models.DateField(auto_now=True, null=True, verbose_name='Дата обновления записи')
+    photo = models.ImageField(upload_to='image%Y/%m/%d', null=True, verbose_name='Фото')
+    exist = models.BooleanField(default=True, verbose_name='Существует ли в каталоге?')
 
     def __str__(self):
         return self.name
@@ -17,4 +17,5 @@ class Pizza(models.Model):
         verbose_name = 'Pizza'
         verbose_name_plural = 'Pizzas'  # Имя для приложения отображения
         ordering = ['name']  # sort fields alphabetically if ['-name']
+
 
